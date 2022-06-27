2005040107a85ccd5358115879a3e837118703ab
Luis Castillo <ldcastillodev@gmail.com>
2022-06-27 15:18:55 -0400

    texto modificado

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world with express')
+  res.send('<h1> express with html </h1>')
 })
 
 
 
-app.listen(3000);
+server.listen(3000);
 console.log('server on port 3000')
